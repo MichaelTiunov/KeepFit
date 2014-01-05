5a2327d1d899cc42744c7b329320c5e35d26b7a1
miketiunov <tiunovmike@gmail.com>
2014-01-05 21:47:35 +0400

    Add phone app
    phone app for communication with web api

---
+++ KeepFit/KeepFit/KeepFit.Phone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using KeepFit.Phone.Resources;
+
+namespace KeepFit.Phone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
